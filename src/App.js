f3db36733e75838cf3d2cdc08b7a7da9ec914ecf
yukta-patel <yuktapatel.infynno@gmail.com>
2020-05-01 17:58:20 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './app.css';
+// import laptop from './laptop.jpg'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div id="container" className="container">
+        <div id="left-container">
+          <h1>Login</h1>
+          <p>Don't have an account? <span>Creat your account</span>, it takes less<br></br>than a minute</p>
+          <input type="text" className="input_userId" placeholder="Username"></input>
+          <input type="text" className="input_pw" placeholder="Password"></input>
+
+          <div className="sub-container">
+            <div><input type="checkbox" className="check_box"></input></div>
+            <div className="remember_me">Remember Me</div>
+            <div><a href="#" className="forgot_pw">Forgot Password?</a></div>
+          </div>
+
+          <div><button className="btn_login">LOGIN</button></div>
+
+          <div className="bottom_container">
+            <div className="login_with">Or Login With</div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+
+        <div id="right-container" class="wrapper">
+          <img src="./laptop.jpg" alt="laptop image"></img>
+        </div>
     </div>
+    
   );
 }
 
