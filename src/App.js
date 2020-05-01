1954c4d6afa205e45eaf740b3a7d3c529ce571a3
yukta-patel <yuktapatel.infynno@gmail.com>
2020-05-01 18:11:52 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './app.css';
-// import laptop from './laptop.jpg'
+import laptop from './laptop.jpeg'
+import user from './user.jpg'
+import password from './password.jpg'
+import facebook from './facebook.jpg'
+import twitter from './twitter.jpg'
+import google from './google+.jpg'
+
+
 
 
 
@@ -12,8 +19,15 @@ function App() {
         <div id="left-container">
           <h1>Login</h1>
           <p>Don't have an account? <span>Creat your account</span>, it takes less<br></br>than a minute</p>
-          <input type="text" className="input_userId" placeholder="Username"></input>
-          <input type="text" className="input_pw" placeholder="Password"></input>
+          <div className="text-1">
+            <div className="div-user"><input type="text" className="input_userId" placeholder="Username"></input></div>
+            <div><img className="img-user" src={user}></img></div>
+          </div>
+          
+          <div className="text-1">
+            <div className="div-user"><input type="text" className="input_pw" placeholder="Password"></input></div>
+            <div><img className="img-pw" src={password}></img></div>
+          </div>
 
           <div className="sub-container">
             <div><input type="checkbox" className="check_box"></input></div>
@@ -25,18 +39,21 @@ function App() {
 
           <div className="bottom_container">
             <div className="login_with">Or Login With</div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div><img className="img logo-fb" src={facebook}></img></div>
+            <div><img className="img" src={twitter}></img></div>
+            <div><img className="img" src={google}></img></div>
           </div>
         </div>
 
-        <div id="right-container" class="wrapper">
-          <img src="./laptop.jpg" alt="laptop image"></img>
+        <div id="right-container" className="wrapper">
+          <img className="laptop-img" src={laptop} alt="laptop image"></img>
+          <div className="overlay"></div>
+          <div className="header">Welcome</div>
+          <div className="content">Sed do eiusmod temporut labore et<br></br> dolore magna aliqua. Your perfect<br></br>place to buy and sell.</div>
         </div>
     </div>
     
-  );
+  )
 }
 
 export default App;
